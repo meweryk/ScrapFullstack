57a5a536bcd93689b4328b4797d55b3517e04c1c
meweryk <39477539+meweryk@users.noreply.github.com>
2019-09-19 16:17:23 +0300

    register-login-token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/skrapfullstack',
+  mongoURI: 'mongodb+srv://Ivashchenko:i220903110976P%2F%29@cluster0-hc5cb.gcp.mongodb.net/materialsdb?retryWrites=true&w=majority',
   jwt: 'dev-jwt'
 }
