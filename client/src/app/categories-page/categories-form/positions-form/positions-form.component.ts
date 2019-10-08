9127f872e630e08c11ccc85bed3a429a0f9f727c
meweryk <39477539+meweryk@users.noreply.github.com>
2019-10-09 01:16:34 +0300

    add positions 01

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { PositionsService } from 'src/app/shared/services/positions.service';
+import { Position } from 'src/app/shared/interfaces';
+import { MaterialService, MaterialInstance } from 'src/app/shared/classes/material.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-positions-form',
   templateUrl: './positions-form.component.html',
   styleUrls: ['./positions-form.component.css']
 })
-export class PositionsFormComponent implements OnInit {
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
+  @Input('categoryId') categoryId: string
+  @ViewChild('modal', { static: false }) modalRef: ElementRef
 
-  constructor() { }
+  positions: Position[] = []
+  loading = false
+  modal: MaterialInstance
+  form: FormGroup
+
+  constructor(private positionsService: PositionsService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      cost: new FormControl(null, [Validators.required, Validators.min(1)])
+    })
+    this.loading = true
+    this.positionsService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions
+      this.loading = false
+    })
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  onSelectPosition(position: Position) {
+    this.modal.open()
+  }
+
+  onAddPosition() {
+    this.modal.open()
+  }
+  onDeletePosition(position: Position) {
+
+  }
+
+  onCancel() {
+    this.modal.close()
+  }
+
+  onSubmit() {
+    this.form.disable()
+
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+    this.positionsService.create(newPosition).subscribe(
+      position => {
+        MaterialService.toast('Позиция создана')
+        this.positions.push(position)
+      },
+      error => MaterialService.toast(error.error.message),
+      () => {
+        this.modal.close()
+        this.form.reset({ name: '', cost: 1 })
+        this.form.enable()
+      }
+    )
   }
 
 }
