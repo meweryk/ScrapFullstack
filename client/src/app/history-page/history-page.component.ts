3086263d10d84bed3728639b3925fead4c065440
meweryk <39477539+meweryk@users.noreply.github.com>
2019-10-12 13:01:25 +0300

    загрузка элементов

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,17 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { OrdersServise } from '../shared/services/orders.service';
+import { Subscription } from 'rxjs';
+import { Order } from '../shared/interfaces';
+
+const STEP = 2
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  @ViewChild('tooltip', { static: false }) tooltipRef: ElementRef
+  tooltip: MaterialInstance
+  oSub: Subscription
   isFilterVisible = false
+  orders: Order[] = []
 
-  constructor() { }
+  offset = 0
+  limit = STEP
+
+  loading = false
+  reloading = false
+
+  constructor(private ordersService: OrdersServise) { }
 
   ngOnInit() {
+    this.reloading = true
+    this.fetch()
+  }
+
+  private fetch() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    }
+    this.oSub = this.ordersService.fetch(params).subscribe(orders => {
+      this.orders = this.orders.concat(orders)
+      this.loading = false
+      this.reloading = false
+    })
+  }
+
+  loadMore() {
+    this.offset += STEP
+    this.loading = true
+    this.fetch()
+  }
+
+  ngOnDestroy() {
+    this.tooltip.destroy()
+    this.oSub.unsubscribe()
+  }
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef)
   }
 
 }
