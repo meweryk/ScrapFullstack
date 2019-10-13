dc25f2fe980302c58a0ad617fd99683a74fd1e34
meweryk <39477539+meweryk@users.noreply.github.com>
2019-10-13 21:26:54 +0300

    Вычисление данных аналитики

--- controllers/order.js
+++ controllers/order.js
@@ -1,66 +1,66 @@
-const Order = require( '../models/Order' )
-const errorHandler = require( '../utils/errorHandler' )
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
 
 //localhost:5000/api/order?offset=2&limit=5
-module.exports.getAll = async function ( req, res ) {
+module.exports.getAll = async function (req, res) {
   const query = {
     user: req.user.id
   }
 
   //дата старта
-  if ( req.query.start ) {
+  if (req.query.start) {
     query.date = {
       //больше или равно
       $gte: req.query.start
     }
   }
 
-  if ( req.query.end ) {
-    if ( !query.date ) {
+  if (req.query.end) {
+    if (!query.date) {
       query.date = {}
     }
 
-    query.date[ '$lte' ] = req.query.end
+    query.date['$lte'] = req.query.end
   }
 
-  if ( req.query.order ) {
+  if (req.query.order) {
     query.order = +req.query.order
   }
 
   try {
     const orders = await Order
-      .find( query )
-      .sort( {
+      .find(query)
+      .sort({
         date: -1
-      } )
-      .skip( +req.query.offset )
-      .limit( +req.query.limit )
+      })
+      .skip(+req.query.offset)
+      .limit(+req.query.limit)
 
-    res.status( 200 ).json( orders )
+    res.status(200).json(orders)
 
-  } catch ( e ) {
-    errorHandler( res, e )
+  } catch (e) {
+    errorHandler(res, e)
   }
 }
 
-module.exports.create = async function ( req, res ) {
+module.exports.create = async function (req, res) {
   try {
-    const lastOrder = await Order.findOne( {
+    const lastOrder = await Order.findOne({
       user: req.user.id
-    } ).sort( {
+    }).sort({
       date: -1
-    } )
+    })
 
     const maxOrder = lastOrder ? lastOrder.order : 0
 
-    const order = await new Order( {
+    const order = await new Order({
       list: req.body.list,
       user: req.user.id,
       order: maxOrder + 1
-    } ).save( )
+    }).save()
 
-    res.status( 201 ).json( order )
-  } catch ( e ) {
-    errorHandler( res, e )
+    res.status(201).json(order)
+  } catch (e) {
+    errorHandler(res, e)
   }
 }
