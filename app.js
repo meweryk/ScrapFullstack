f3ac496771d78497f48a07f3762f12a07af3708b
meweryk <39477539+meweryk@users.noreply.github.com>
2019-10-16 17:37:28 +0300

    подготовка сервера

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
@@ -33,5 +34,17 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 
+//добавляется после production build (npm run build)
+if (process.env.NODE === 'production') {
+  app.use(express.static('client/dist/client'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname, 'client', 'dist', 'client', 'index.html'
+      )
+    )
+  })
+}
 
 module.exports = app
