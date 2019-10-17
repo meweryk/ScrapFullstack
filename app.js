5c4ad5f1b69fee5c2c9e5d2610c09b6f27e50f4d
meweryk <39477539+meweryk@users.noreply.github.com>
2019-10-17 18:41:40 +0300

    initialize

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/api/position', positionRoutes)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist/client'))
 
-  app.get('*', (req, res) => {
+  app.get('*', (_req, res) => {
     res.sendFile(
       path.resolve(
         __dirname, 'client', 'dist', 'client', 'index.html'
