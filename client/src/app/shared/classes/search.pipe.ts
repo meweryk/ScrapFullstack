b1501fabf2985d2de8f70757c3227b7b0c1061f7
meweryk <39477539+meweryk@users.noreply.github.com>
2020-01-01 22:19:29 +0200

    filter order

--- client/src/app/shared/classes/search.pipe.ts
+++ client/src/app/shared/classes/search.pipe.ts
@@ -6,10 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class SearchPipe implements PipeTransform {
     transform(positions, value) {
-        //value[0] = new RegExp(value[0], 'i')
-        //value[1] = new RegExp(value[1], 'i')
         return positions.filter(position => {
-            return (position.name.includes(value[0]) && position.shop.includes(value[1]))
+            return (position.name.toLocaleUpperCase().includes(value[0].toLocaleUpperCase()) && position.shop.toLocaleUpperCase().includes(value[1].toLocaleUpperCase()))
         })
     }
 }
