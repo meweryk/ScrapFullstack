b308b9a9f0b1ecfc8b3256ad780dd6c2fb8151a8
meweryk <39477539+meweryk@users.noreply.github.com>
2020-02-07 15:51:15 +0200

    service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private swUpdate: SwUpdate) { }
 
   ngOnInit() {
     const potentialToken = localStorage.getItem('auth-token')
@@ -23,6 +24,14 @@ export class AppComponent implements OnInit {
     const thisShop = localStorage.getItem('my-shop')
     if (thisShop !== null) {
       this.auth.setShop(thisShop)
+    }
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("Доступна новая версия. Загрузить новую версию?")) {
+          window.location.reload()
+        }
+      })
     }
   }
 }
